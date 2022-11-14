8de75d05d8b4bc38f60c1e2a2eb502b88f9a3dad
lixin <3318395483@qq.com>
2022-11-14 10:14:19 +0800

    优化了create_leg_rig的代码，实现了生成ribbon_rig的功能

--- rigging/leg_rig.py
+++ rigging/leg_rig.py
@@ -25,12 +25,17 @@ import ikfk_rig
 # leg_bp_joints = ['bpjnt_l_hip_001', 'bpjnt_l_knee_001',' bpjnt_l_ankle_001','bpjnt_l_ball_001','bpjnt_l_toe_001']
 
 class Leg_Rig(ikfk_rig.IKFK_Rig):
-    def __init__(self, bp_joints = None , joint_parent = None, control_parent = None,mirror = True):
+    def __init__(self, bp_joints = None , joint_parent = None, control_parent = None,mirror = True,ribbon = True,joint_number = 5):
         super(Leg_Rig, self).__init__(bp_joints = bp_joints, joint_parent=joint_parent,control_parent = control_parent)
         self.mirror = mirror
         self.leg_bp_joints = self.get_modular_bp_joints(self.leg_rig)
         self.bp_joints = self.leg_bp_joints
-
+        self.ribbon = ribbon
+        self.joint_number = joint_number
 
     def create_leg_rig(self):
         self.create_ikfk_chain_rig()
+        if self.ribbon  == True:
+            self.ribbon_Rig(self.ikfk_chain, self.joint_number)
+            self.ribbon_Rig(self.ikfk_chain_mirror,  self.joint_number)
+
