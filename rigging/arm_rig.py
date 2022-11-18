1bc840b366f556178ef726527325b56be2ff8aed
lixin <3318395483@qq.com>
2022-11-18 18:00:08 +0800

    添加了锁骨，肩胛骨和肩膀耸肩的控制器

--- rigging/arm_rig.py
+++ rigging/arm_rig.py
@@ -26,19 +26,76 @@ reload(jointUtils)
 import ikfk_rig
 
 
-arm_bp_joints = ['bpjnt_l_shoulder_001', 'bpjnt_l_elbow_001',' bpjnt_l_wrist_001']
+arm_bp_joints = ['jnt_l_shoulder_001', 'jnt_l_elbow_001',' jnt_l_wrist_001']
 
 class Arm_Rig(ikfk_rig.IKFK_Rig):
     def __init__(self, bp_joints = None , joint_parent = None, control_parent = None,mirror = True,ribbon = True,joint_number = 5):
         super(Arm_Rig, self).__init__(bp_joints = bp_joints, joint_parent=joint_parent,control_parent = control_parent)
         self.mirror = mirror
-        self.arm_bp_joints = self.get_modular_bp_joints(self.arm_rig)
+        self.arm_bp_joints = ['bpjnt_l_shoulder_001', 'bpjnt_l_elbow_001',' bpjnt_l_wrist_001']
         self.bp_joints = self.arm_bp_joints
         self.ribbon = ribbon
         self.joint_number = joint_number
+        self.clavicle_jnt = 'jnt_l_clavicle_001'
+        self.scapula_jnt = 'jnt_l_scapula_001'
+        self.shrug_jnt = 'jnt_l_shrug_001'
+        self.clavicle_mirror = cmds.mirrorJoint(self.clavicle_jnt, mirrorYZ = True, mirrorBehavior = True,
+                                                 searchReplace = ['_l_', '_r_'])
+
 
 
     def create_arm_rig(self):
+        # 创建锁骨的控制器
+        clavicle_ctrl = self.clavicle_jnt.replace('jnt_','ctrl_')
+        clavicle_ctrl_obj = controlUtils.Control.create_ctrl(clavicle_ctrl, shape = 'clavicle', radius = 13, axis = 'Z+',
+                                                            pos = self.clavicle_jnt, parent =self.cog_ctrl.replace('ctrl_','output_'))
+        # 创建肩胛骨的控制器
+        scapula_ctrl = self.scapula_jnt.replace('jnt_', 'ctrl_')
+        scapula_ctrl_obj = controlUtils.Control.create_ctrl(scapula_ctrl,
+                                                         shape = 'Cube', radius = 6, axis = 'Z+',
+                                                         pos = None, parent = clavicle_ctrl.replace('ctrl_','output_'))
+        cmds.matchTransform(scapula_ctrl.replace('ctrl_', 'zero_'), self.scapula_jnt, position = True)
+
+        # 创建肩膀耸肩的控制器
+        shrug_ctrl = self.shrug_jnt.replace('jnt_', 'ctrl_')
+        shrug_ctrl_obj = controlUtils.Control.create_ctrl(shrug_ctrl,
+                                                         shape = 'shrug', radius = 6, axis = 'X+',
+                                                         pos = None, parent =self.cog_ctrl)
+
+        cmds.matchTransform(shrug_ctrl.replace('ctrl_','zero_'),self.shrug_jnt,position = True)
+
+        # 创建镜像锁骨的控制器
+        clavicle_ctrl_mirror = clavicle_ctrl.replace('_l_','_r_')
+        clavicle_ctrl_mirror_obj = controlUtils.Control.create_ctrl(clavicle_ctrl_mirror, shape = 'clavicle', radius = 13, axis = 'Z+',
+                                                            pos = self.clavicle_jnt.replace('_l_','_r_'), parent = self.cog_ctrl.replace('ctrl_','output_'))
+        cmds.setAttr(clavicle_ctrl_mirror.replace('ctrl_','offset_') + '.scaleX',-1)
+        cmds.setAttr(clavicle_ctrl_mirror.replace('ctrl_', 'offset_') + '.scaleY', -1)
+        cmds.setAttr(clavicle_ctrl_mirror.replace('ctrl_', 'offset_') + '.scaleZ', -1)
+        # 创建镜像肩胛骨的控制器
+        scapula_ctrl_mirror = scapula_ctrl.replace('_l_','_r_')
+        scapula_ctrl_mirror_obj = controlUtils.Control.create_ctrl(scapula_ctrl_mirror,
+                                                         shape = 'Cube', radius = 6, axis = 'Z+',
+                                                         pos = None, parent = clavicle_ctrl_mirror.replace('ctrl_','output_'))
+        cmds.matchTransform(scapula_ctrl_mirror.replace('ctrl_', 'zero_'), self.scapula_jnt.replace('_l_','_r_'), position = True)
+
+        # 创建镜像肩膀耸肩的控制器
+        shrug_ctrl_mirror = shrug_ctrl.replace('_l_','_r_')
+        shrug_ctrl_mirror_obj = controlUtils.Control.create_ctrl(shrug_ctrl_mirror,
+                                                         shape = 'shrug', radius = 6, axis = 'X+',
+                                                         pos = None, parent = self.cog_ctrl)
+
+        cmds.matchTransform(shrug_ctrl_mirror.replace('ctrl_','zero_'),self.shrug_jnt.replace('_l_','_r_'),position = True)
+
+        # 创建约束
+        ctrls = [clavicle_ctrl,scapula_ctrl,clavicle_ctrl_mirror,scapula_ctrl_mirror]
+        for ctrl in ctrls:
+            cmds.parentConstraint(ctrl,ctrl.replace('ctrl_','jnt_'),mo = True)
+
+        # 创建肩膀耸肩的目标约束
+        cmds.aimConstraint(shrug_ctrl,clavicle_ctrl.replace('ctrl_','driven_'),mo = True)
+        cmds.aimConstraint(shrug_ctrl_mirror, clavicle_ctrl_mirror.replace('ctrl_', 'driven_'), mo = True)
+
+
         self.create_ikfk_chain_rig()
         if self.ribbon  == True:
             self.create_ribbon_Rig(self.ikfk_chain,self.control_parent, self.joint_number)
