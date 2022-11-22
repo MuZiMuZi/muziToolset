d87b5781a56cdddcffeb16bfe627634882d17d47
lixin <3318395483@qq.com>
2022-11-22 16:37:18 +0800

    创建了绑定系统的面板，并且连接对应的槽函数

--- res/ui/rigging_modular/rigging_widget.py
+++ res/ui/rigging_modular/rigging_widget.py
@@ -27,7 +27,7 @@ except ImportError:
 
 import maya.cmds as cmds
 import muziToolset.conf.config as config
-reload(backGround)
+import muziToolset.rigging.build_rig as build_rig
 
 
 
@@ -61,6 +61,7 @@ class RiggingWindow(QWidget):
         self.modular_listView = self.ui.modular_listView
         #连接导入按钮
         self.import_button = self.ui.import_button
+        self.import_button.clicked.connect(self.clicked_import_button)
         #连接关节方向的设置
         self.jointDirection_label = self.ui.jointDirection_label
         self.jointDirection_true_radioButton = self.ui.jointDirection_true_radioButton
@@ -72,6 +73,7 @@ class RiggingWindow(QWidget):
 
         #连接清除的设置
         self.clear_button = self.ui.clear_button
+        self.clear_button.clicked.connect(self.clicked_clear_button)
 
         #连接属性的lsitview
         self.property_listWidget = self.ui.property_listWidget
@@ -93,15 +95,26 @@ class RiggingWindow(QWidget):
 
         #连接控制器重置的设置
         self.reset_button = self.ui.reset_button
+        self.reset_button.clicked.connect(self.clicked_reset_button)
 
         #连接创建绑定的设置
         self.build_button = self.ui.build_button
+        self.build_button.clicked.connect(self.clicked_build_button)
 
+    def clicked_import_button(self):
+        build = build_rig.Build_Rig()
+        build.import_modular()
 
+    def clicked_clear_button(self):
+        build = build_rig.Build_Rig()
+        build.delete_modular()
 
+    def clicked_reset_button(self):
+        pass
 
-
-
+    def clicked_build_button(self):
+        build = build_rig.Build_Rig()
+        build.build_rig()
 
 #
 def show():
