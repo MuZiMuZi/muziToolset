de6b1e9d436ba1619da1a4c48bb906a2ba3b45ab
lixin <3318395483@qq.com>
2023-05-09 16:15:00 +0800

    写了一个base类型的绑定，只有一个控制器和关节结构

---
+++ rigging/base/base.py
@@ -0,0 +1,32 @@
+import maya.cmds as cmds
+from . import bone
+from importlib import reload
+
+
+
+reload(bone)
+class Base(bone.Bone):
+	u"""
+	基础的关节和控制器绑定
+	"""
+
+	def __init__(self , side , name , index , joint_parent = None , control_parent = None , point_value = True ,
+	             orient_value = True , scale_value = True , mo_value = True) :
+		bone.Bone.__init__(self,side , name , index , joint_parent , control_parent , point_value , orient_value ,
+		                 scale_value , mo_value)
+		self._rtype = 'base'
+		self._name = name + self._rtype
+	
+	
+	
+	def set_shape(self , shape = 'circle') :
+		super().set_shape(shape)
+		self.shape = 'cube'
+	
+
+	
+		
+	
+
+	
+	
