3f4889bb7525450994de5c26f0679c382b343c37
lixin <3318395483@qq.com>
2023-05-09 16:14:28 +0800

    定义了一个bone的基础类，作为关节和控制器的绑定结构继承下去

--- rigging/base/bone.py
+++ rigging/base/bone.py
@@ -2,19 +2,21 @@
 from importlib import reload
 from ...core import jointUtils
 from ...core import pipelineUtils
+from ...core import hierarchyUtils
+from ...core import controlUtils
 import maya.cmds as cmds
 
 
 
 reload(pipelineUtils)
-
+reload(hierarchyUtils)
 
 
 class Bone(object) :
 	"""
 	创建定位的bp关节，然后生成对应的绑定
 	创建绑定的步骤:
-	1. build_bpjnt: 	创建定位的bp关节
+	1. build_setup: 	创建定位的bp关节
 	2. build_rig: 		根据定位的bp关节创建关节
 
 	# Attribute:
@@ -35,7 +37,8 @@ class Bone(object) :
 		:param control_parent: 生成的控制器的父层级
 		"""
 		super(Bone , self).__init__()
-		
+		#创建层级结构
+		hierarchyUtils.Hierarchy.create_rig_grp()
 		self._side = side
 		self._name = name
 		self._index = index
@@ -44,17 +47,9 @@ class Bone(object) :
 		self.joint_parent = joint_parent
 		self.control_parent = control_parent
 		if not self.joint_parent :
-			if not cmds.objExists('_joint') :
-				cmds.createNode('transform' , name = '_joint')
-				self.joint_parent = '_joint'
-			else :
-				self.joint_parent = '_joint'
+			self.joint_parent = '_joint'
 		if not self.control_parent :
-			if not cmds.objExists('_control') :
-				cmds.createNode('transform' , name = '_control')
-				self.control_parent = '_control'
-			else :
-				self.control_parent = '_control'
+			self.control_parent = '_control'
 				
 		# 约束的参数设置
 		self.point_value = point_value
@@ -62,8 +57,8 @@ class Bone(object) :
 		self.scale_value = scale_value
 		self.mo_value = mo_value
 		
+		#生成的绑定类型
 		self._rtype = None
-	
 	
 	
 	@property
@@ -107,22 +102,28 @@ class Bone(object) :
 		根据定位的bp关节创建关节
 		'''
 		self.jnt_list = []
-		for bpjnt in self.bpjnt_list :
-			self.jnt = cmds.createNode('joint' , name = bpjnt.replace('bpjnt' , 'jnt') , parent = self.joint_parent)
-			cmds.delete(bpjnt)
+		for i in range(self._index) :
+			self.jnt_name = 'jnt_{}_{}_{:03d}'.format(self._side , self._name , i + 1)
+			self.jnt = cmds.createNode('joint' , name = self.jnt_name , parent = self.joint_parent)
+			self.bpjnt = self.jnt_name.replace('jnt','bpjnt')
+			cmds.matchTransform(self.jnt_name,self.bpjnt)
 			self.jnt_list.append(self.jnt)
+			cmds.delete(self.bpjnt)
 	
 	
-	
+	def set_shape(self,shape = 'circle'):
+		self.shape = shape
+		
 	def create_ctrl(self) :
 		u"""
 		创建控制器
 		"""
 		self.ctrl_list = []
-		for jnt in self.jnt_list :
-			self.ctrl = controlUtils.Control.create_ctrl(jnt.replace('jnt' , 'ctrl') , shape = 'cube' ,
+		for i in range(self._index) :
+			self.ctrl_name = 'ctrl_{}_{}_{:03d}'.format(self._side , self._name , i + 1)
+			self.ctrl = controlUtils.Control.create_ctrl(self.ctrl_name , shape = self.shape ,
 			                                             radius = 5 ,
-			                                             axis = 'X+' , pos = jnt , parent = self.control_parent)
+			                                             axis = 'X+' , pos = self.ctrl_name.replace('ctrl','jnt') , parent = self.control_parent)
 			self.ctrl_list.append(self.ctrl)
 	
 	
@@ -141,9 +142,9 @@ class Bone(object) :
 	
 	
 	
-	def build_bp(self) :
+	def build_setup(self) :
 		"""
-		创建bp的定位关节
+		创建bp的定位关节,生成准备
 		"""
 		self.create_bpjnt()
 	
@@ -154,12 +155,10 @@ class Bone(object) :
 		创建绑定系统
 		"""
 		self.create_joint()
+		self.set_shape('circle')
 		self.create_ctrl()
 		self.add_constraint()
 
 
 
-if __name__ == "__main__" :
-	custom = bone.Bone(side = 'l' , name = 'zz' , index = 5 , joint_parent = None , control_parent = None)
-	custom.build_bp()
-	custom.build_rig()
+
