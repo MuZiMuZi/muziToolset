2775ff2c4d6f31bdfdda0724aaa532cb41806664
lixin <3318395483@qq.com>
2023-05-11 15:06:55 +0800

    add_constraint构筑完成，在关节和控制器之间添加平滑衰减约束

--- rigging/chain/chainEP.py
+++ rigging/chain/chainEP.py
@@ -35,14 +35,13 @@ class ChainEP(base.Base):
 		for p in percents :
 			integer = int(round(p * (self.joint_number - 1)))
 			self.cvs.append(integer)
-	
-	
+		print(self.cvs)
 	
 	def create_bpjnt(self) :
 		"""
 		创建定位的bp关节
 		"""
-		bpjnt_list = pipelineUtils.Pipeline.create_joints_on_curve(self.curve , self.ctrl_number)
+		bpjnt_list = pipelineUtils.Pipeline.create_joints_on_curve(self.curve , self.joint_number)
 		for joint_number , bpjnt in enumerate(bpjnt_list) :
 			bpjnt = cmds.rename(bpjnt , self.bpjnt_list[joint_number])
 			cmds.parent(bpjnt,self.joint_parent)
@@ -62,26 +61,50 @@ class ChainEP(base.Base):
 	
 	
 	
+	def create_ctrl(self) :
+		self.set_shape(self.shape)
+		for index in (self.cvs)  :
+			self.ctrl = controlUtils.Control.create_ctrl(self.ctrl_list[index] , shape = self.shape ,
+			                                             radius = self.radius ,
+			                                             axis = 'X+' , pos = self.jnt_list[index] ,
+			                                             parent = self.control_parent)
+	
+	
+	
 	def add_constraint(self) :
 		'''
 		在关节和控制器之间添加平滑衰减约束
 		'''
-		for i in range(len(self.cvs) - 1) :
-			head = self.cvs[i]
-			tail = self.cvs[i + 1]
-			for j in range(head , tail + 1) :
-				gap = 1.00 / (tail - head)
+		for index in self.cvs :
+			#开头的关节进行约束
+			if index == 0 :
+				cmds.pointConstraint(self.ctrl_list[index] , self.jnt_list[index] ,
+				                     mo = 1)
+				cmds.orientConstraint(self.ctrl_list[index] , self.jnt_list[index] ,
+				                      mo = 1)
 				
-				# 制作点约束和旋转约束
-				cmds.pointConstraint(self.ctrl_list[head] , self.jnt_list[j] ,
-				                     w = 1 - ((j - head) * gap) , mo = 1)
-				cmds.pointConstraint(self.ctrl_list[tail] , self.jnt_list[j] ,
-				                     w = (j - head) * gap , mo = 1)
-				cmds.orientConstraint(self.ctrl_list[head] , self.jnt_list[j] ,
-				                      w = 1 - ((j - head) * gap) , mo = 1)
-				cmds.orientConstraint(self.ctrl_list[tail] , self.jnt_list[j] ,
-				                      w = (j - head) * gap , mo = 1)
-	
+			# 结尾的关节进行约束
+			elif index == self.cvs[-1]:
+				cmds.pointConstraint(self.ctrl_list[index] , self.jnt_list[-1] ,
+				                     mo = 1)
+				cmds.orientConstraint(self.ctrl_list[index] , self.jnt_list[- 1])
+			# 中间的关节进行约束
+			else :
+				for index in range(len(self.cvs) - 1) :
+					head = self.cvs[index]
+					tail = self.cvs[index + 1]
+					for jnt in range(head , tail + 1) :
+						gap = 1.00 / (tail - head)
+						
+						# 设置前后两端的控制器影响关节的权重值
+						cmds.pointConstraint(self.ctrl_list[head] , self.jnt_list[jnt] ,
+						                     w = 1 - ((jnt - head) * gap) , mo = 1)
+						cmds.pointConstraint(self.ctrl_list[tail] , self.jnt_list[jnt] ,
+						                     w = (jnt - head) * gap , mo = 1)
+						cmds.orientConstraint(self.ctrl_list[head] , self.jnt_list[jnt] ,
+						                      w = 1 - ((jnt - head) * gap) , mo = 1)
+						cmds.orientConstraint(self.ctrl_list[tail] , self.jnt_list[jnt] ,
+						                      w = (jnt - head) * gap , mo = 1)
 	
 	
 	
