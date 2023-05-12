cb9b2a6b21d2db3398006cc631143df24f857df7
lixin <3318395483@qq.com>
2023-05-12 18:26:31 +0800

    create_bpjnt的方法重写，创建定位的bp关节

--- rigging/module/test.py
+++ rigging/module/test.py
@@ -1,21 +1,17 @@
-import muziToolset.rigging.module.spine as spine
+import muziToolset.rigging.module.foot as foot
 from importlib import reload
 
 
 
-reload(spine)
+reload(foot)
 def build_setup():
-    spine_m = spine.Spine(side = 'm', name = 'zz' , joint_number = 4 , direction = [0, 1 , 0] , length = 10 ,
-                          is_stretch = 1,joint_parent = None ,
-                         control_parent = None)
-    spine_m.build_setup()
+    foot_l = foot.Foot(side = 'l' , name  = 'zz', joint_parent = None , control_parent = None)
+    foot_l.build_setup()
 def build_rig():
-    spine_m = spine.Spine(side = 'm' , name = 'zz', joint_number = 4 , direction = [0 , 1 , 0] , length = 10 ,
-                          is_stretch = 1,joint_parent = None ,
-                         control_parent = None)
-    spine_m.build_rig()
+    foot_l = foot.Foot(side = 'l' , name = 'zz' , joint_parent = None , control_parent = None)
+    foot_l.build_rig()
 
-
-
+#
+#
 build_setup()
 build_rig()
