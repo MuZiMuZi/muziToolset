9b02fd9e2ca6d1459bca79c757822e9693a347f9
lixin <3318395483@qq.com>
2023-05-12 17:54:03 +0800

    create_bpjnt的方法重写，创建定位的bp关节

---
+++ rigging/module/foot.py
@@ -0,0 +1,66 @@
+import maya.cmds as cmds
+from ..base import base , bone
+from ..chain import chain
+from ...core import controlUtils , jointUtils , hierarchyUtils,vectorUtils
+
+
+
+class Foot(chain.Chain) :
+	
+	
+	
+	def __init__(self , side , name , joint_number = 3 , length = 6 , joint_parent = None , control_parent = None) :
+		super().__init__(side , name , joint_number , joint_parent , control_parent)
+		self.length = length
+		self.parts = ['Ankle' , 'Ball' , 'Toe']
+		self.rvs_jnt_list = list()
+		self.rvs_jnt_parts = ['Tiptoe' , 'Heel' , 'Inn' , 'Out']
+		
+		self.value = length/joint_number
+	
+	def create_namespace(self) :
+		u"""
+		创建名称规范整理
+		"""
+		for part in self.parts :
+			self.bpjnt_list.append('bpjnt_{}_{}{}_001'.format(self._side , self._name , part))
+			self.jnt_list.append('jnt_{}_{}{}_001'.format(self._side , self._name , part))
+			self.zero_list.append('zero_{}_{}{}_001'.format(self._side , self._name , part))
+			self.driven_list.append('driven_{}_{}{}_001'.format(self._side , self._name , part))
+			self.ctrl_list.append('ctrl_{}_{}{}_001'.format(self._side , self._name , part))
+			self.output_list.append('output_{}_{}{}_001'.format(self._side , self._name , part))
+		self.ctrl_grp = ('grp_{}_{}{}_001'.format(self._side , self._name , self._rtype))
+		# 创建用来定位旋转轴心点的关节
+		for part in self.rvs_jnt_parts :
+			self.rvs_jnt_list.append('jntRvs_{}_{}{}_001'.format(self._side , self._name , part))
+		self.rvs_tiptoe_jnt = ('jntRvs_{}_{}Tiptoe_001'.format(self._side , self._name))
+		self.rvs_heel_jnt = ('jntRvs_{}_{}Heel_001'.format(self._side , self._name))
+		self.rvs_inn_jnt = ('jntRvs_{}_{}Inn_001'.format(self._side , self._name))
+		self.rvs_out_jnt = ('jntRvs_{}_{}Out_001'.format(self._side , self._name))
+		
+
+	
+	
+	
+	def create_bpjnt(self) :
+		"""
+		创建定位的bp关节
+		"""
+		for bpjnt in self.bpjnt_list :
+			self.bpjnt = cmds.createNode('joint' , name = bpjnt , parent = self.joint_parent)
+			# 指定关节的父层级为上一轮创建出来的关节
+			self.joint_parent = self.bpjnt
+			# 调整距离
+			cmds.setAttr(bpjnt + '.translate' , 0 , 0 , self.value)
+		cmds.setAttr(self.bpjnt_list[0] + '.translate' , self.value * self.side_value , self.value , 0)
+		# 创建用来定位ik旋转轴心点的关节
+		for rvs_jnt in self.rvs_jnt_list :
+			self.rvs_jnt = cmds.createNode('joint' , name = rvs_jnt)
+		# 进行关节定向
+		jointUtils.Joint.joint_orientation(self.rvs_jnt_list)
+		
+		# 设置定位旋转轴心点的关节的位置
+		cmds.setAttr(self.rvs_tiptoe_jnt + '.translate' , self.value * self.side_value , 0 , self.value * 2)
+		cmds.setAttr(self.rvs_heel_jnt + '.translate' , self.value * self.side_value , 0 , 0)
+		cmds.setAttr(self.rvs_inn_jnt + '.translate' , 0 , 0 , self.value)
+		cmds.setAttr(self.rvs_out_jnt + '.translate' , self.value * 2 * self.side_value , 0 , self.value)
