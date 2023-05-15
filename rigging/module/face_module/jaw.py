1778d6302208a093dd51d4ca98ccd72fb0a842e6
lixin <3318395483@qq.com>
2023-05-15 16:11:23 +0800

    jaw,下巴的绑定系统创建完成

--- rigging/module/face_module/jaw.py
+++ rigging/module/face_module/jaw.py
@@ -26,6 +26,7 @@ class Jaw(chainFK.ChainFK) :
 		self.radius = 2
 	
 	
+	
 	def create_namespace(self) :
 		super().create_namespace()
 	
@@ -36,3 +37,21 @@ class Jaw(chainFK.ChainFK) :
 		self.jaw_bpjnt_path = os.path.abspath(__file__ + "/../jaw_bpjnt.ma")
 		# 导入关节
 		cmds.file(self.jaw_bpjnt_path , i = True , rnn = True)
+
+
+
+if __name__ == "__main__" :
+	def build_setup() :
+		jaw_m = jaw.Jaw(side = 'm' , joint_parent = None , control_parent = None)
+		jaw_m.build_setup()
+	
+	
+	
+	def build_rig() :
+		jaw_m = jaw.Jaw(side = 'm' , joint_parent = None , control_parent = None)
+		jaw_m.build_rig()
+	
+	
+	
+	build_setup()
+	build_rig()
