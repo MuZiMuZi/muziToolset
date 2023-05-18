1e257408f4cbe19559cb0b51ad30d0b8e804383a
lixin <3318395483@qq.com>
2023-05-18 09:34:36 +0800

    重新格式化代码

--- rigging/module/face_module/brow.py
+++ rigging/module/face_module/brow.py
@@ -232,3 +232,21 @@ class Brow(base.Base) :
 		self.create_folice()
 		self.create_ctrl()
 		self.add_constraint()
+
+
+
+if __name__ == "__main__" :
+	def build_setup() :
+		brow_m = brow.Brow(side = 'm' , joint_parent = None , control_parent = None)
+		brow_m.build_setup()
+	
+	
+	
+	def build_rig() :
+		brow_m = brow.Brow(side = 'm' , joint_parent = None , control_parent = None)
+		brow_m.build_rig()
+	
+	
+	
+	build_setup()
+	build_rig()
