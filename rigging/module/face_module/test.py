0898f4f8f51cbef5666393f3f1c17607c34cf409
lixin <3318395483@qq.com>
2023-05-19 17:59:06 +0800

    移动了位置，进行版本管理

--- rigging/module/face_module/test.py
+++ rigging/module/face_module/test.py
@@ -1,33 +1,25 @@
-
-import muziToolset.rigging.module.face_module.eyeLid as eyeLid
+import muziToolset.rigging.module.face_module.eye as eye
 from importlib import reload
 
 
 
-reload(eyeLid)
+reload(eye)
 
 
 
 def build_setup() :
-	eye_lid = eyeLid.EyeLid(side = 'l' , name = 'upper' , joint_number = 7 , joint_parent = None , control_parent = None)
-	eye_lid.build_setup()
+	eye_l = eye.Eye(side = 'l' , name = '' , joint_number = 2 , length = 10 , joint_parent = None ,
+	                control_parent = None)
+	eye_l.build_setup()
 
 
-
-def build_curve() :
-	eye_lid = eyeLid.EyeLid(side = 'l' , name = 'upper' , joint_number = 7 , joint_parent = None , control_parent =
-	None)
-	eye_lid.build_setup()
-	eye_lid.build_curve()
 
 def build_rig() :
-	eye_lid = eyeLid.EyeLid(side = 'l' , name = 'upper' , joint_number = 7 , joint_parent = None , control_parent =
-	None)
-	eye_lid.build_rig()
+	eye_l = eye.Eye(side = 'l' , name = '' , joint_number = 2 , length = 10 , joint_parent = None ,
+	                control_parent = None)
+	eye_l.build_rig()
 
 
 
-
-# build_setup()
-build_curve()
+build_setup()
 build_rig()
