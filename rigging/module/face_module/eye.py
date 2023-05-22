ca9ea5f6679b6507a9981a1c2b9de58f5b490b38
lixin <3318395483@qq.com>
2023-05-22 11:38:40 +0800

    eye构筑完成，眼皮与眼睛的绑定制作好了

--- rigging/module/face_module/eye.py
+++ rigging/module/face_module/eye.py
@@ -296,13 +296,15 @@ class Eye(chain.Chain) :
 
 if __name__ == "__main__" :
 	def build_setup() :
-		eye_l = eye.Eye(side = 'l' , joint_parent = None , control_parent = None)
+		eye_l = eye.Eye(side = 'l' , name = '' , joint_number = 2 , length = 10 , joint_parent = None ,
+		                control_parent = None)
 		eye_l.build_setup()
 	
 	
 	
 	def build_rig() :
-		eye_l = eye.Eye(side = 'l' , joint_parent = None , control_parent = None)
+		eye_l = eye.Eye(side = 'l' , name = '' , joint_number = 2 , length = 10 , joint_parent = None ,
+		                control_parent = None)
 		eye_l.build_rig()
 	
 	
