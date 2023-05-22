b742305067edae68ca1ee3b9f175df6cc8deec50
lixin <3318395483@qq.com>
2023-05-22 17:59:10 +0800

    mouth.py基本构筑完成，接着需要写拉链嘴巴的绑定

--- rigging/module/face_module/mouth.py
+++ rigging/module/face_module/mouth.py
@@ -73,12 +73,16 @@ class Mouth(bone.Bone) :
 		# 创建嘴内侧与嘴外侧的控制器
 		
 		self.inn_ctrl = controlUtils.Control.create_ctrl(self.inn_ctrl , shape = 'ball' , axis = 'X+' , radius = 0.1 ,
-		                                                 pos = self.mouth_lip_upper.zero_list[0] , parent = self.ctrl_grp)
+		                                                 pos = self.mouth_lip_upper.zero_list[0] ,
+		                                                 parent = self.ctrl_grp)
 		self.out_ctrl = controlUtils.Control.create_ctrl(self.out_ctrl , shape = 'ball' , axis = 'X+' , radius = 0.1 ,
-		                                                 pos = self.mouth_lip_lower.zero_list[-1] , parent = self.ctrl_grp)
-	
+		                                                 pos = self.mouth_lip_lower.zero_list[-1] ,
+		                                                 parent = self.ctrl_grp)
+		
 		# 整理控制器的层级结构
-		cmds.parent(self.mouth_lip_upper.ctrl_grp,self.mouth_lip_lower.ctrl_grp,self.ctrl_grp)
+		cmds.parent(self.mouth_lip_upper.ctrl_grp , self.mouth_lip_lower.ctrl_grp , self.ctrl_grp)
+	
+	
 	
 	def add_constraint(self) :
 		# 创建上下嘴唇的约束
@@ -97,3 +101,24 @@ class Mouth(bone.Bone) :
 		# 隐藏上下嘴唇的外侧控制器
 		cmds.setAttr(self.mouth_lip_upper.zero_list[-1] + '.v' , 0)
 		cmds.setAttr(self.mouth_lip_lower.zero_list[-1] + '.v' , 0)
+
+
+
+if __name__ == '__main__' :
+	def build_setup() :
+		mouth_m = mouth.Mouth(side = 'm' , name = '' , joint_number = 2 , joint_parent = None ,
+		                      control_parent = None)
+		mouth_m.build_setup()
+	
+	
+	
+	def build_rig() :
+		mouth_m = mouth.Mouth(side = 'm' , name = '' , joint_number = 2 , joint_parent = None ,
+		                      control_parent = None)
+		mouth_m.build_rig()
+	
+	
+	
+	#
+	build_setup()
+	build_rig()
