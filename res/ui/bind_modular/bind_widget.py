3c17d448851c8a6416da4ab9bdcab6a29c53dcf4
lixin <3318395483@qq.com>
2023-05-25 11:53:36 +0800

    add_connect方法添加，用来添加连接的槽函数

--- res/ui/bind_modular/bind_widget.py
+++ res/ui/bind_modular/bind_widget.py
@@ -25,17 +25,33 @@ class BindSystemWindow(bindSystem.Ui_MainWindow , QtWidgets.QMainWindow) :
 		super().__init__(*args , **kwargs)
 		# 调用父层级的创建ui方法
 		self.setupUi(self)
+		self.proxy_path = 'D:/'
 		self.apply_model()
-	
+		
+		self.add_connect()
 	
 	
 	def apply_model(self) :
 		u"""
 		添加模型到view里
 		"""
-
-
-
+		# # 添加模型到proxy_view里
+		# self.proxy_model = QtWidgets.QFileSystemModel()
+		# self.proxy_view.setModel(self.proxy_model)
+		# self.proxy_view.setRootIndex(self.proxy_path)
+		# root_index= self.proxy_model.index(self.proxy_path)
+		# self.proxy_view.setRootIndex(root_index)
+		#
+		# 添加模型到proxy_view里
+		self.proxy_model = QtGui.QStandardItemModel()
+		self.proxy_view.setModel(self.proxy_model)
+	
+	def add_connect(self):
+		u"""
+		用来添加连接的槽函数
+		"""
+		self.proxy_view.doubleClicked.connect(self.cmd_proxy_view_dbclk)
+		
 
 
 if __name__ == '__main__' :
