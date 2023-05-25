bbb8ae2b49bd30f0e3372bdec44e48ef39b0edb6
lixin <3318395483@qq.com>
2023-05-25 09:45:59 +0800

    准备开发绑定系统的工具面板

--- res/ui/bind_modular/bind_widget.py
+++ res/ui/bind_modular/bind_widget.py
@@ -1,1 +1,22 @@
-from
+# coding=utf-8
+# 导入sys模块为了防止程序运行崩溃
+import sys
+# ui界面生成需要三个模块QtWidgets，QtGui，QtCore
+from PySide2 import QtWidgets
+from PySide2 import QtGui
+from PySide2 import QtCore
+
+from . import bindSystem
+
+
+
+class BindSystemWindow(bindSystem.Ui_MainWindow , QtWidgets.QMainWindow) :
+	u'''
+	用于创建绑定系统的界面系统
+	'''
+	
+	
+	
+	def __init__(*args , **kwargs) :
+		super().__init__(*args , **kwargs)
+	
