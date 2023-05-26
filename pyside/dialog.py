e5be86f5e41ea3b8a94f57f30a54f8ce493f48aa
lixin <3318395483@qq.com>
2023-05-26 16:03:03 +0800

    create_widgets 和create_layouts，添加了小部件

--- pyside/dialog.py
+++ pyside/dialog.py
@@ -20,9 +20,33 @@ class TestDialog(QtWidgets.QDialog) :
 	
 	def __init__(self , parent = maya_main_window()) :
 		super(TestDialog , self).__init__(parent)
+		# 设置标题和尺寸
 		self.setWindowTitle('TestDialog')
 		self.setMinimumHeight(300)
 		self.setMinimumWidth(300)
+		
+		# 添加部件
+		self.create_widgets()
+		self.create_layouts()
+	
+	
+	
+	def create_widgets(self) :
+		self.lineEdit = QtWidgets.QLineEdit()
+		self.checkBox1 = QtWidgets.QCheckBox('checkBox1')
+		self.checkBox2 = QtWidgets.QCheckBox('checkBox2')
+		self.button1 = QtWidgets.QPushButton('button1')
+		self.button2 = QtWidgets.QPushButton('button2')
+	
+	
+	
+	def create_layouts(self) :
+		self.main_layout = QtWidgets.QVBoxLayout(self)
+		self.main_layout.addWidget(self.lineEdit)
+		self.main_layout.addWidget(self.checkBox1)
+		self.main_layout.addWidget(self.checkBox2)
+		self.main_layout.addWidget(self.button1)
+		self.main_layout.addWidget(self.button2)
 
 
 
