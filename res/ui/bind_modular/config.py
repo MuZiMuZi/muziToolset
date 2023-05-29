96c20dd8785b26aa1a09adc9f1897f4d2fb07d73
lixin <3318395483@qq.com>
2023-05-29 11:17:35 +0800

    ui的配置文件导入bind库,配置好项目目录路径

--- res/ui/bind_modular/config.py
+++ res/ui/bind_modular/config.py
@@ -6,8 +6,7 @@ project_root = os.path.dirname(__file__)
 ui_dir = os.path.join(project_root , 'ui')
 icon_dir = os.path.join(project_root , 'icon')
 
-print(project_root)
-
-print(ui_dir)
-
-print(icon_dir)
+bind_root = os.path.abspath(__file__ + "/../../../../bind")
+base_dir = os.path.join(bind_root , 'base')
+chain_dir = os.path.join(bind_root , 'chain')
+module_dir = os.path.join(bind_root , 'module')
