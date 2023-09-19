0866549ac633e11f32b4e7452b35da101e308c9c
li xin <3318395483@qq.com>
2023-09-19 13:53:04 +0800

    添加了Tools的文件包，用于重新开发工具集

---
+++ Tools/config.py
@@ -0,0 +1,9 @@
+import os
+from enum import Enum , unique
+
+
+
+project_root = os.path.dirname(__file__)
+ui_dir = os.path.join(project_root , 'ui')
+icon_dir = os.path.join(project_root , 'icon')
+
--- res/ui/mesh_modular/mesh_widget.py
+++ res/ui/mesh_modular/mesh_widget.py
@@ -50,31 +50,42 @@ class Selector_tool :
     def __init__ (self) :
         self.winTitle = 'Select_mesh_Face'
         self.winName = 'MeshSelectTool'
-        self.objects - list()
+        self.objects = list ()
 
 
     def init_ui (self) :
         """
         创建ui界面
         """
-        # 创建一个columnLayout来获取物体的名称
+        # 创建一个frameLayout来获取物体的名称
         # columnWidth3设置三个部件的宽度，adjustableColumn表示第几个部件跟随着窗口缩放
         # placeholderText提示语
-        with pm.columnLayout (adj = True) :
-            with pm.menuBarLayout () :
+        with pm.frameLayout () as self.mainForm :
+            with pm.menuBarLayout () as self.menuBar :
                 self.add_menus ()
-            pm.textFieldButtonGrp (
+            self.nameField = pm.textFieldButtonGrp (
                 label = 'Objects' ,
                 columnWidth3 = [50 , 140 , 5] ,
                 adjustableColumn = 2 ,
                 editable = False ,
                 buttonLabel = '<' ,
                 placeholderText = 'Please select meshes in the viewport' ,
-                buttonCommand = lambda *a : None
+                buttonCommand = self.insert_selection
             )
 
-        # self.win.show()
+            with pm.scrollLayout (childResizable = True) as self.mainScroll :
+                pass
 
+        #设置menuBar的布局吸附
+        self.mainForm.attachFrom(self.menuBar.name(),'top',0)
+        self.mainForm.attachFrom (self.menuBar.name () , 'left' , 0)
+        self.mainForm.attachFrom (self.menuBar.name () , 'right' , 0)
+
+        #设置nameField的布局吸附
+        self.mainForm.attachFrom (self.nameField.name () , 'right' , 0)
+        self.mainForm.attachFrom (self.nameField.name () , 'right' , 0)
+        self.mainForm.attachFrom (self.nameField.name () , 'right' , 0)
+        self.mainForm.attachFrom (self.nameField.name () , 'right' , 0)
 
     def add_menus (self) :
         '''
@@ -87,7 +98,7 @@ class Selector_tool :
             pm.menuItem (label = 'Collapse All' , image = 'dot.png' , command = lambda *a : None)
             pm.menuItem (label = 'Expand All' , image = 'dot.png' , command = lambda *a : None)
             pm.menuItem (label = 'Close' , image = 'closeTabButton.png' ,
-                         command = lambda *a :workspaceControl (self.winTitle , edit = True , close = True))
+                         command = lambda *a : workspaceControl (self.winTitle , edit = True , close = True))
 
         with pm.menu (label = 'Edit' , tearOff = True) as self.editMenu :
             # 创建分隔符
@@ -103,11 +114,12 @@ class Selector_tool :
                                                             absolute = True))
 
 
-    def insert_selection(self,*args):
-        pass
+    def add_buttons (self) :
+        for i in range (5) :
+            pm.button ()
 
 
-    def validate_selection (self , *args):
+    def validate_selection (self , *args) :
         """
         检查所选择的物体是否为模型节点
         """
@@ -117,3 +129,18 @@ class Selector_tool :
             return True
         else :
             return False
+
+
+    def insert_selection (self , *args) :
+        """
+        根据所检查的结果来对物体进行操作
+        """
+        # 判断如果所选择的物体都不具有模型节点则返回
+        if not self.validate_selection () :
+            return
+        for object in self.objects :
+            with pm.frameLayout (
+                    label = object.getShape ().name () ,
+                    collapsable = True ,
+                    parent = self.mainScroll) :
+                self.add_buttons ()
