f2e440fbc26a61bd55e4e823667d43779be37130
li xin <3318395483@qq.com>
2023-09-19 10:04:06 +0800

    adv嘴唇中间添加新的次级控制器，应用于动画制作抿嘴的情况

--- core/advUtils.py
+++ core/advUtils.py
@@ -9,10 +9,10 @@ from . import pipelineUtils , controlUtils , hierarchyUtils
 import maya.cmds as cmds
 
 
-class AdvUtils ( object ) :
+class AdvUtils (object) :
 
     @staticmethod
-    def add_lip_ctrl ( ) :
+    def add_lip_ctrl () :
         '''
         adv嘴唇中间添加新的次级控制器，应用于动画制作抿嘴的情况
         思路：
@@ -22,28 +22,28 @@ class AdvUtils ( object ) :
         '''
 
         # 分成上下嘴唇两种情况制作次级控制器
-        for type in [ 'upperLip' , 'lowerLip' ] :
+        for type in ['upperLip' , 'lowerLip'] :
             lip_joint = type + 'RibbonJoint_M'
             lip_surface = type + 'CenterPlane'
             lip_main_ctrl = type + '_M'
 
             # 删除lip关节上自带的约束
-            cmds.select ( lip_joint , r = True )
-            pipelineUtils.Pipeline.delete_constraints ( )
+            cmds.select (lip_joint , r = True)
+            pipelineUtils.Pipeline.delete_constraints ()
 
             # 创建新的次级控制器并且吸附到lip关节上
-            lip_ctrl = controlUtils.Control.create_ctrl ( name = 'ctrl_m_{}Lip_001'.format ( type ) , shape = 'Cube' ,
-                                                          radius = 0.5 , axis = 'Y+' , pos = lip_joint ,
-                                                          parent = 'LipRegion_M' )
+            lip_ctrl = controlUtils.Control.create_ctrl (name = 'ctrl_m_{}Lip_001'.format (type) , shape = 'Cube' ,
+                                                         radius = 0.5 , axis = 'Y+' , pos = lip_joint ,
+                                                         parent = 'LipRegion_M')
 
             # 创建出来的次级控制器对lip关节进行约束
-            pipelineUtils.Pipeline.create_constraint ( lip_ctrl.replace ( 'ctrl' , 'output' ) , lip_joint ,
-                                                       point_value = True , orient_value = True ,
-                                                       scale_value = False ,
-                                                       mo_value = True )
+            pipelineUtils.Pipeline.create_constraint (lip_ctrl.replace ('ctrl' , 'output') , lip_joint ,
+                                                      point_value = True , orient_value = True ,
+                                                      scale_value = False ,
+                                                      mo_value = True)
             # lip总控制器对创建出来的次级控制器组做约束
-            pipelineUtils.Pipeline.create_constraint ( lip_main_ctrl ,
-                                                       lip_ctrl.replace ( 'ctrl' , 'driven' ) ,
-                                                       point_value = True , orient_value = True ,
-                                                       scale_value = False ,
-                                                       mo_value = True )
+            pipelineUtils.Pipeline.create_constraint (lip_main_ctrl ,
+                                                      lip_ctrl.replace ('ctrl' , 'driven') ,
+                                                      point_value = True , orient_value = True ,
+                                                      scale_value = False ,
+                                                      mo_value = True)
