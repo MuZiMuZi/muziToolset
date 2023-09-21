00448ec0e993decb13452934003b42da123e9fbe
li xin <3318395483@qq.com>
2023-09-21 14:44:25 +0800

    开发关节工具集的ui界面

--- Tools/Joint_Tool_main.py
+++ Tools/Joint_Tool_main.py
@@ -6,6 +6,7 @@ import pymel.core as pm
 from .config import ui_dir , icon_dir
 from ..core import pipelineUtils , nameUtils , jointUtils
 from importlib import reload
+
 import maya.cmds as cmds
 
 
@@ -28,19 +29,21 @@ class Joint_Tool () :
 
         pm.scrollLayout ()
         # 创建装备fk的ui界面
-        pm.frameLayout (label = 'create_FK(装配FK)' ,
-                        collapsable = True)
+        pm.frameLayout (label = '装配FK' ,
+                        collapsable = True ,
+                        backgroundColor = [0 , 0, 20])
 
         pm.rowColumnLayout (numberOfColumns = 2)
 
-        pm.button (label = 'create_fk(创建fk)' , command = lambda *a : print ('create_fk(创建fk)'))
-        pm.button (label = 'delete_fk(删除fk)' , command = lambda *a : print ('delete_fk(删除fk)'))
+        pm.button (label = '创建fk' , command = lambda *a : print ('create_fk(创建fk)'))
+        pm.button (label = '删除fk' , command = lambda *a : print ('delete_fk(删除fk)'))
         pm.setParent ('..')
         pm.setParent ('..')
 
         # 创建装备ik的ui界面
-        pm.frameLayout (label = 'create_IK(装配IK)' ,
-                        collapsable = True)
+        pm.frameLayout (label = '装配IK' ,
+                        collapsable = True ,
+                        backgroundColor = [0 , 0, 20])
         pm.textFieldButtonGrp (label = '起始关节' ,
                                columnWidth3 = [50 , 140 , 5] ,
                                adjustableColumn = 2 ,
@@ -60,10 +63,12 @@ class Joint_Tool () :
         pm.button (label = 'create_IK(创建IK)' , command = lambda *a : None)
         pm.button (label = 'create_IK(删除IK)' , command = lambda *a : None)
         pm.setParent ('..')
+        pm.setParent ('..')
 
         # 创建装备ik链条的ui界面
-        pm.frameLayout (label = 'create_IK_Spine(装配IK链条绑定)' ,
-                        collapsable = True)
+        pm.frameLayout (label = '装配IK链条绑定' ,
+                        collapsable = True ,
+                        backgroundColor = [0 , 0, 20])
         pm.textFieldButtonGrp (label = '起始关节' ,
                                columnWidth3 = [50 , 140 , 5] ,
                                adjustableColumn = 2 ,
@@ -89,21 +94,39 @@ class Joint_Tool () :
 
         pm.rowColumnLayout (numberOfColumns = 2)
 
-        pm.button (label = 'create_IK(创建IK)' , command = lambda *a : None)
-        pm.button (label = 'delete_IK(删除IK)' , command = lambda *a : None)
+        pm.button (label = '创建IK链条' , command = lambda *a : None)
+        pm.button (label = '删除IK链条' , command = lambda *a : None)
+        pm.setParent ('..')
         pm.setParent ('..')
 
         # 创建关节小工具的ui界面
-        pm.frameLayout (label = 'joint_tool(关节小工具)' ,
-                        collapsable = True)
+        pm.frameLayout (label = '关节小工具' ,
+                        collapsable = True ,
+                        backgroundColor = [0 , 0, 20])
         # 设置关节显示
-        self.jnt_size = pm.floatSliderGrp ('jnt_Size' , label = 'jnt_size(关节显示大小):' , f = True , min = 1 , max = 10 , fmn = 1 ,
-                           fmx = 100 ,
-                           v = 1)
+        self.jnt_size = pm.floatSliderGrp ('jnt_Size' , label = '关节显示大小:' , f = True , min = 1 ,
+                                           max = 10 , fmn = 1 ,
+                                           fmx = 100 ,
+                                           v = 1)
         pm.rowColumnLayout (numberOfColumns = 3)
-        pm.button (label = 'create_IK(创建IK)' , command = lambda *a : None)
-        pm.button (label = 'create_IK(创建IK)' , command = lambda *a : None)
-        pm.button (label = 'create_IK(创建IK)' , command = lambda *a : None)
+        cmds.iconTextButton (style = 'iconAndTextHorizontal' , image1 = 'menuIconModify.png' , label = '显示关节轴向' ,
+                             command = lambda *a : None)
+        cmds.iconTextButton (style = 'iconAndTextHorizontal' , image1 = 'menuIconModify.png' , label = '隐藏关节轴向' ,
+                             command = lambda *a : None)
+        cmds.iconTextButton (style = 'iconAndTextHorizontal' , image1 = 'kinJoint.png' , label = '关节定向清零' ,
+                             command = lambda *a : None)
+
+        cmds.iconTextButton (style = 'iconAndTextHorizontal' , image1 = 'kinInsert.png' , label = '创建关节' ,
+                             command = lambda *a : None)
+        cmds.iconTextButton (style = 'iconAndTextHorizontal' , image1 = 'kinInsert.png' , label = '创建子关节' ,
+                             command = lambda *a : None)
+        cmds.iconTextButton (style = 'iconAndTextHorizontal' , image1 = 'kinInsert.png' , label = '组成关节链' ,
+                             command = lambda *a : None)
+
+        cmds.iconTextButton (style = 'iconAndTextHorizontal' , image1 = 'kinJoint.png' , label = '开启分段比例补偿' ,
+                             command = lambda *a : None)
+        cmds.iconTextButton (style = 'iconAndTextHorizontal' , image1 = 'kinJoint.png' , label = '关闭分段比例补偿' ,
+                             command = lambda *a : None)
 
         self.main_window.show ()
 
