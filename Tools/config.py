832ce2b986f62ea710826119b9292525600e0a6c
li xin <3318395483@qq.com>
2023-09-22 16:33:51 +0800

    更新配置文件

--- Tools/config.py
+++ Tools/config.py
@@ -1,12 +1,21 @@
+from __future__ import unicode_literals
 import os
 from enum import Enum , unique
 
 
+#############################################################
+# 配置模块
+#############################################################
 
-project_root = os.path.dirname(__file__)
+
+# 路径
+project_root = os.path.dirname (__file__)
 data_dir = os.path.abspath (__file__ + "/../image")
 ui_dir = os.path.abspath (__file__ + "/../ui")
 icon_dir = os.path.abspath (__file__ + "/../icon")
 
+# 是否处于DEBUG状态
+DEBUG = True
+VERSION = 'v0.1'
 
-print(icon_dir)
+# 全局变量
