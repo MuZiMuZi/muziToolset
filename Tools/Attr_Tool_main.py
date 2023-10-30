7ea1569cef388bd279dba20396bbe436a6216c6a
li xin <3318395483@qq.com>
2023-10-30 09:14:34 +0800

    属性工具箱优化了按钮的文字提示

--- Tools/Attr_Tool_main.py
+++ Tools/Attr_Tool_main.py
@@ -35,18 +35,25 @@ class Attr_Tool (QWidget) :
         self.attr_window_label = QLabel ('----------------属性编辑----------------')
         self.attr_window_label.setStyleSheet (u"color: rgb(255, 0, 0);")
         self.add_attr_window_btn = QPushButton (QIcon (icon_dir + '/add.png') , 'Add_Attribute(添加属性)')
+        self.add_attr_window_btn.setToolTip('打开添加属性窗口')
         self.edit_attr_window_btn = QPushButton (QIcon (icon_dir + '/edit.png') , 'Edit_Attribute(编辑属性)')
+        self.edit_attr_window_btn.setToolTip ('打开编辑属性窗口')
         self.connect_attr_window_btn = QPushButton (QIcon (icon_dir + '/connect-empty.png') ,
                                                     'Connect_Attr(连接编辑器)')
+        self.connect_attr_window_btn.setToolTip('打开连接编辑器')
         self.channel_control_window_btn = QPushButton (QIcon (icon_dir + '/control.png') , 'Channel_Control(通道控制)')
+        self.channel_control_window_btn.setToolTip ('打开通道控制编辑器')
         self.delete_attr_window_btn = QPushButton (QIcon (icon_dir + '/delete.png') , 'Delete_Attr(删除属性)')
+        self.delete_attr_window_btn.setToolTip ('删除选中的属性')
 
         # 创建属性工具的页面部件
         self.attr_tool_label = QLabel ('----------------属性工具----------------')
         self.attr_tool_label.setStyleSheet (u"color: rgb(255,170, 255);")
         self.attr_move_label = QLabel('Select a single attribute for displacement(选择单个属性进行位移)')
         self.attr_up_btn = QPushButton ('attr_up(属性上移)')
+        self.attr_up_btn.setToolTip('使选中的属性在通道盒上移')
         self.attr_down_btn = QPushButton ('attr_down(属性下移)')
+        self.attr_down_btn.setToolTip ('使选中的属性在通道盒下移')
 
         # 创建属性设置的页面部件
         self.attr_set_label = QLabel ('----------------属性设置----------------')
@@ -73,7 +80,9 @@ class Attr_Tool (QWidget) :
 
         # 设置按钮
         self.attr_set_btn = QPushButton ('set(设置)')
+        self.attr_set_btn.setToolTip('根据选择的属性设置来设置属性的锁定和隐藏')
         self.attr_reset_btn = QPushButton ('reset(重置)')
+        self.attr_reset_btn.setToolTip('重置选择的属性设置')
 
         self.attr_cheekbox = [self.translation_locked_cheekbox , self.translation_hidden_cheekbox ,
 
