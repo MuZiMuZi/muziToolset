479ab0eb8b43c92a55f0acfdd2c5bd8b0aca8aff
li xin <3318395483@qq.com>
2023-11-23 11:09:47 +0800

    创建了绑定系统的新ui，添加编码

---
+++ bind/Bind_Tool_main.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+import os
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+from ..core import pipelineUtils , nameUtils
+from . import Bind_Tool
+from importlib import reload
+import maya.cmds as cmds
+
+
+reload (Bind_Tool)
+
+
+class Bind_Tool (Bind_Tool.Ui_MainWindow , QMainWindow) :
+
+    def __init__ (self , parent = None) :
+        super ().__init__ (parent)
+        # 调用父类的ui方法，来运行ui
+        self.winTitle = 'Bind_Tool(命名工具)'
+        self.setupUi (self)
+
+
+def show () :
+    try :
+        bind_Tool.close ()
+        bind_Tool.deleteLater ()
+    except :
+        pass
+    bind_Tool = Bind_Tool ()
+
+    bind_Tool.show ()
+
+
+if __name__ == '__main__' :
+    # 通过QApplication方法来生成应用
+    app = QApplication ()
+    qt_app = Bind_Tool ()
+    qt_app.show ()
+    app.exec_ ()
