76036afa12883c7e8e1087713eb186dfa4028288
li xin <3318395483@qq.com>
2023-12-07 09:17:47 +0800

    绑定系统的代码层级结构整理

--- bind/test.py
+++ bind/test.py
@@ -1,6 +1,11 @@
-from muziToolset.res.ui.bind_modular import bind_widget
+from muziToolset.bind.Bind_Tool_main import Bind_Tool_main
 from importlib import reload
-reload(bind_widget)
+reload(Bind_Tool_main)
 
-window = bind_widget.BindSystemWindow()
-window.show()
+try :
+    win.close ()  # 为了不让窗口出现多个，因为第一次运行还没初始化，所以要try，在这里尝试先关闭，再重新新建一个窗口
+    win.deleteLater ()
+except :
+    pass
+win = Bind_Tool_main.Bind_Widget ()
+win.show ()
