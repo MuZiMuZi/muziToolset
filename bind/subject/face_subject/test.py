7030e57af56dd0afd350ff4420ac38c945925d60
li xin <3318395483@qq.com>
2023-12-15 18:23:40 +0800

    测试眉毛绑定

--- bind/subject/face_subject/test.py
+++ bind/subject/face_subject/test.py
@@ -7,3 +7,15 @@ eye_lid_upper = eyeLid.EyeLid (side = 'l' , name = 'upper' , joint_number = 7 , 
                                control_parent = None)
 # eye_lid_upper.build_setup ()
 eye_lid_upper.build_rig ()
+
+
+
+import muziToolset.bind.subject.face_subject.brow as brow
+from importlib import reload
+
+
+reload (brow)
+brow_m = brow.Brow (side = 'm' , joint_parent = None , control_parent = None)
+brow_m.build_setup ()
+brow_m.build_rig ()
+
