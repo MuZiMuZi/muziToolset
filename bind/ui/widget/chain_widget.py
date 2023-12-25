d95c53115dd4e995e1475e9183504a70683dd29e
li xin <3318395483@qq.com>
2023-12-25 17:20:48 +0800

    重新优化base_ui的组件缩放策略，用来优化界面显示

--- bind/ui/widget/chain_widget.py
+++ bind/ui/widget/chain_widget.py
@@ -1,43 +1,100 @@
-from ...config import Direction
-from . import chain_ui
-from . import base_widget
+# coding=utf-8
+# 导入所有需要的模块
+
+from __future__ import unicode_literals , print_function
+
+
+try :
+	from PySide2.QtCore import *
+	from PySide2.QtGui import *
+	from PySide2.QtWidgets import *
+	from PySide2 import __version__
+	from shiboken2 import wrapInstance
+
+except ImportError :
+	from PySide.QtCore import *
+	from PySide.QtGui import *
+	from PySide.QtWidgets import *
+	from PySide import __version__
+	from shiboken import wrapInstance
 from importlib import reload
+import os
 from ... import config
+from . import chain_ui
+from ..widget import bone_widget
+from ....bind.module.base import base
+from ....core import qtUtils
 
 
-reload(base_widget)
-reload(chain_ui)
+reload (bone_widget)
+reload (base)
+reload (chain_ui)
 
 
+class Chain_Widget (chain_ui.Ui_MainWindow , QMainWindow) :
 
-class Chain_Widget(base_widget.Base_Widget , chain_ui.Ui_MainWindow) :
-	
-	
-	
-	def __init__(self , name = 'chain') :
+
+	def __init__ (self , parent = None , *args , **kwargs) :
 		'''
-		使用设置初始化QListWidgetItem，如名称和图标，以及初始化base、额外的widget对象和ui文件，也对应要构建的绑定组件对象
+        使用设置初始化QListWidgetItem，如名称和图标，以及初始化base、额外的widget对象和ui文件，也对应要构建的绑定组件对象
 
-		'''
-		super(base_widget.Base_Widget , self).__init__(name)
-		self.base_ui = 'chain.ui'
-		self.init_base()
-	
-	
-	
-	def init_base(self) :
+        '''
+		super ().__init__ (parent , *args , **kwargs)
+		# 调用父类的ui方法，来运行ui
+		self.setupUi (self)
+
+		self.name = None
+		self.side = None
+		self.jnt_number = None
+		self.jnt_parent = None
+		self.control_parent = None
+		self.init_base ()
+
+
+	# self.create_widget ()
+	# self.create_layout ()
+
+
+	def init_base (self) :
+		# 初始化参数
+		for side in config.Side :
+			self.side_cbox.addItem (side.value)
+
+
+	def add_connect (self) :
+		u"""
+        用来添加连接的槽函数
+        """
+
+
+	# self.create_btn.clicked.connect (lambda :print(12))
+	# self.base_widget.delete_btn.clicked.connect (self.delete_setup)
+
+
+	def parse_base (self , *args) :
 		"""
-		初始化作为QWidget对象的base_widget属性,用于设置绑定的基础属性（例如名称，边，关节数量，关节的父对象，控制器的父对象）
-		"""
-		super().init_base()
-		# 添加direction的combox
-		for direction in Direction :
-			self.base_widget.direction_cbox.addItem(str(direction.value))
-	
-	
-	
+        分析base_widget中的输入并将其作为参数返回
+        """
+		self.name = self.name_edit.text ()
+		self.side = self.side_cbox.currentText ()
+		self.jnt_number = self.jnt_number_sbox.currentText ()
+		self.jnt_parent = self.jnt_parent_edit.text ()
+		self.control_parent = self.control_parent_edit.text ()
+
+
+	def build_setup (self) :
+		# 读取输入信息
+		self.parse_base ()
+	#
+	# # 生成定位关节系统
+	# self.setup = base.Base (self.side , self.name , self.joint_number , self.joint_parent , self.control_parent)
+	# self.setup.build_setup ()
 	def parse_extra(self) :
 		length = self.extra_widget.length_sbox.value()
 		direction = ast.literal_eval(self.extra_widget.direction_cbox.currentText())
 		
 		return [length , direction]
+
+
+def main () :
+	return Base_Widget ()
