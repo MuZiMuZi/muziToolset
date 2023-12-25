b2359b4389afc7a89a901c28ffc13bb041c02dad
li xin <3318395483@qq.com>
2023-12-25 15:55:34 +0800

    重新优化config的代码，让代码更加易懂

--- bind/config.py
+++ bind/config.py
@@ -2,34 +2,86 @@ import os
 from enum import Enum , unique
 
 
+# 项目根目录
+project_root = os.path.dirname (__file__)
 
-project_root = os.path.dirname(__file__)
-ui_dir = os.path.join(project_root , 'ui/widget')
+# UI文件存放目录
+ui_dir = os.path.join (project_root , 'ui/widget')
+
+# 图标文件存放目录
 icon_dir = os.path.abspath (__file__ + "/../../tools/icon")
 
-bind_root = os.path.abspath(__file__ + "/..")
-base_dir = os.path.join(bind_root , 'module/base')
-chain_dir = os.path.join(bind_root , 'module/chain')
-limb_dir = os.path.join(bind_root , 'module/limb')
-module_dir = os.path.join(bind_root , 'module')
+# 绑定模块根目录
+bind_root = os.path.abspath (__file__ + "/..")
+
+# base模块目录
+base_dir = os.path.join (bind_root , 'module/base')
+
+# chain模块目录
+chain_dir = os.path.join (bind_root , 'module/chain')
+
+# limb模块目录
+limb_dir = os.path.join (bind_root , 'module/limb')
+
+# 所有模块的父目录
+module_dir = os.path.join (bind_root , 'module')
+
+# QSS文件存放目录
 qss_dir = os.path.abspath (__file__ + "/../../tools/qss")
 
 
 @unique
-class Side(Enum) :
-	# 定义边
-	left = 'l'
-	right = 'r'
-	middle = 'm'
-
+class Side (Enum) :
+    """
+    定义模块的方向。
+    """
+    left = 'l'
+    right = 'r'
+    middle = 'm'
 
 
 @unique
-class Direction(Enum) :
-	# 定义轴向
-	y_positive = [0 , 1 , 0]
-	y_negative = [0 , -1 , 0]
-	x_POSITIVE = [1 , 0 , 0]
-	x_negative = [-1 , 0 , 0]
-	z_POSITIVE = [0 , 0 , 1]
-	z_negative = [0 , 0 , -1]
+class Direction (Enum) :
+    """
+    定义模块的轴向。
+    """
+    y_positive = [0 , 1 , 0]
+    y_negative = [0 , -1 , 0]
+    x_positive = [1 , 0 , 0]
+    x_negative = [-1 , 0 , 0]
+    z_positive = [0 , 0 , 1]
+    z_negative = [0 , 0 , -1]
+
+
+def Rigtype (value) :
+    """
+    根据给定的值返回相应的绑定类型。
+
+    参数:
+        value (str): 要查询的值。
+
+    返回:
+        str or None: 如果找到匹配的绑定类型，则返回相应的键；如果未找到匹配，则返回 None。
+    """
+    # 定义不同的绑定类型的值列表
+    rigtype_custom = ['base' , 'master' , 'brow' , 'nose' , 'cheek' , 'jaw']
+    rigtype_chain = ['chainFK' , 'chainIK' , 'chainIKFK' , 'finger' , 'spine']
+    rigtype_chainEP = ['chainEP']
+    rigtype_limb = ['arm' , 'leg' , 'hand' , 'tail' , 'spine']
+    rigtype_face = ['eye' , 'mouth']
+
+    # 创建一个包含绑定类型的字典
+    rigtype_dict = {
+        'custom' : rigtype_custom ,
+        'chain' : rigtype_chain ,
+        'chainEP' : rigtype_chainEP ,
+        'limb' : rigtype_limb ,
+        'face' : rigtype_face
+    }
+
+    # 遍历字典，查找值所在的绑定类型
+    for key , val_list in rigtype_dict.items () :
+        if value in val_list :
+            return key  # 如果找到匹配的值，返回相应的键
+
+    return None  # 如果未找到匹配的值，返回 None
