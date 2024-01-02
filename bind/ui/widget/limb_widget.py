3ef94e71f16b0a3e3b81ef9cf1e842143a384158
li xin <3318395483@qq.com>
2024-01-02 16:07:13 +0800

    parse_base修改方法，获取需要输入的值

--- bind/ui/widget/limb_widget.py
+++ bind/ui/widget/limb_widget.py
@@ -2,14 +2,11 @@
 # 导入所有需要的模块
 from importlib import reload
 
-import maya.cmds as cmds
 from PySide2.QtWidgets import *
 
 from . import base_widget
 from ..setup import limb_ui
-from ... import config
-from ....bind.subject.body_subject import arm,leg,spine
-from ....core import pipelineUtils
+from ....bind.subject.body_subject import arm , leg , spine
 
 
 reload (limb_ui)
@@ -39,7 +36,26 @@ class Limb_Widget (limb_ui.Ui_MainWindow , base_widget.Base_Widget , QMainWindow
 
     def parse_base (self) :
         super ().parse_base ()
+        # 获取是否设置ik控制器的值
+        self.ikCtrl_value = self.ikCtrl_cbox.value ()
 
+        # 获取是否设置fk控制器的值
+        self.fkCtrl_value = self.fkCtrl_cbox.value ()
+
+        # 获取是否设置拉伸的值
+        self.stretch_value = self.stretch_cbox.value ()
+
+        # 获取设置上部分ribbon关节的数量
+        self.up_ribbon_value = self.up_ribbon_sbox.value ()
+
+        # 获取设置下部分ribbon关节的数量
+        self.down_ribbon_value = self.down_ribbon_sbox.value ()
+
+        # 将需要检查参数存在的值封装到列表中
+        parameters_to_add = [self.up_ribbon_value , self.down_ribbon_value]
+
+        # 使用 extend() 方法将列表添加到 base_parameters 中，后续通过check_parameters这个方法判断这些参数是否有值
+        self.base_parameters.extend (parameters_to_add)
 
 
     def build_setup (self) :
@@ -47,7 +63,7 @@ class Limb_Widget (limb_ui.Ui_MainWindow , base_widget.Base_Widget , QMainWindow
         if self.limb in self.limb_modules :
             limb_class = self.chain_modules [self.module]
             self.limb = limb_class (self.side , self.name , self.jnt_number , self.direction , self.length ,
-                                        self.jnt_parent , self.ctrl_parent)
+                                    self.jnt_parent , self.ctrl_parent)
 
 
 def main () :
