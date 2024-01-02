071dd8cf422ea616f7798d3a336b82bf55b7852b
li xin <3318395483@qq.com>
2024-01-02 14:22:49 +0800

    优化Limb_Widget的代码结构，减少重复代码

--- bind/ui/widget/limb_widget.py
+++ bind/ui/widget/limb_widget.py
@@ -1,26 +1,15 @@
 # coding=utf-8
 # 导入所有需要的模块
-
-from __future__ import unicode_literals , print_function
-
-
-try :
-    from PySide2.QtCore import *
-    from PySide2.QtGui import *
-    from PySide2.QtWidgets import *
-    from PySide2 import __version__
-    from shiboken2 import wrapInstance
-
-except ImportError :
-    from PySide.QtCore import *
-    from PySide.QtGui import *
-    from PySide.QtWidgets import *
-    from PySide import __version__
-    from shiboken import wrapInstance
-
-from . import base_widget
-from ..setup import limb_ui
 from importlib import reload
+
+import maya.cmds as cmds
+from PySide2.QtWidgets import *
+
+from . import chain_widget
+from ..setup import limb_ui
+from ... import config
+from ....bind.subject.body_subject import arm,leg,spine
+from ....core import pipelineUtils
 
 
 reload (limb_ui)
@@ -29,16 +18,18 @@ reload (base_widget)
 
 class Limb_Widget (limb_ui.Ui_MainWindow , base_widget.Base_Widget , QMainWindow) :
     # # 添加其他模块类型
-    # limb_modules = {
-    #     'arm' : chainFK.ChainFK ,
-    #     'leg' : chainIK.ChainIK ,
-    #     'spine' : chainIKFK.ChainIKFK ,
-    #
-    # }
+    limb_modules = {
+        'arm' : arm.Arm ,
+        'leg' : leg.Leg ,
+        'spine' : spine.Spine ,
+
+    }
 
 
     def __init__ (self , parent = None , *args , **kwargs) :
         '''
+        # 针对身体四肢模块的绑定模块
+        rigtype_limb = ['arm' , 'leg' , 'spine']
         使用设置初始化QListWidgetItem，如名称和图标，以及初始化base、额外的widget对象和ui文件，也对应要构建的绑定组件对象
 
         '''
