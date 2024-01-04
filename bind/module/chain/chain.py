7db89a51ce94a4f4165f7af5c4b33026153ac1eb
li xin <3318395483@qq.com>
2024-01-04 11:15:29 +0800

    重新优化创建定位关节和创建关节的代码

--- bind/module/chain/chain.py
+++ bind/module/chain/chain.py
@@ -17,7 +17,6 @@ except ImportError :
     from PySide.QtWidgets import *
     from PySide import __version__
     from shiboken import wrapInstance
-import os
 import maya.cmds as cmds
 
 from ....core import jointUtils , pipelineUtils , connectionUtils
@@ -28,39 +27,6 @@ from importlib import reload
 reload (connectionUtils)
 reload (jointUtils)
 reload (base)
-
-
-class ChainItem (base.BaseItem) :
-
-    def __init__ (self , name = 'chain') :
-        """Override"""
-        super (ChainItem , self).__init__ (name)
-        self.extra_ui = 'chain.ui'
-        self.init_extra ()
-
-
-    # 初始化作为QWidget对象的extra_widget属性,用于设置绑定组件的特殊属性，比如(长度，朝向，拉伸，IK启用，FK启用,twist启用)等属性
-    # （Length, orientation, stretch, IK enabled, FK enabled, twist enabled）
-
-    def create_extra_widget (self) :
-        """
-        #初始化作为QWidget对象的extra_widget属性,用于设置绑定组件的特殊属性，比如(长度，朝向，拉伸，IK启用，FK启用,twist启用)等属性
-        #（Length, orientation, stretch, IK enabled, FK enabled, twist enabled）
-        """
-        self.extra_widget = QWidget ()
-        uic.loadUi (os.path.join (UI_DIR , self.extra_ui) , self.extra_widget)
-
-        for direction in Direction :
-            self.extra_widget.length_sbox.addItem (str (direction.value))
-
-
-    def parse_extra (self) :
-        """Override"""
-        seg = self.extra_widget.ui_seg_sbox.value ()
-        length = self.extra_widget.ui_len_sbox.value ()
-        direction = ast.literal_eval (self.extra_widget.ui_dir_cbox.currentText ())
-
-        return [seg , length , direction]
 
 
 class Chain (base.Base) :
@@ -85,49 +51,47 @@ class Chain (base.Base) :
         """
         # 设置bpjnt创建出来的位置放置在top_bpjnt_grp的层级下
         self.bpjnt_parent = self.top_bpjnt_grp
+
+        # 根据命名规范创建用来定位的bp关节
         for bpjnt in self.bpjnt_list :
-            # 判断是否已经生成过定位关节，如果没有生成过定位关节的话则生成定位关节
-            if cmds.objExists (bpjnt) :
-                cmds.delete (bpjnt)
-            else :
-                self.bpjnt = cmds.createNode ('joint' , name = bpjnt , parent = self.bpjnt_parent)
-                # 给bp定位关节设置颜色方便识别
-                cmds.setAttr (self.bpjnt + '.overrideEnabled' , 1)
-                cmds.setAttr (self.bpjnt + '.overrideColor' , 13)
-                # 指定关节的父层级为上一轮创建出来的关节
-                self.bpjnt_parent = self.bpjnt
-                # 调整距离,调整关节的朝向距离
-                # 获取关节的位置信息，例如self.interval是关节的长度，self。direction是空间位置比如[0.0, 1, 0.0]
-                # 他们相乘获得到distance，将关节为位移到对应的位置信息上
-                distance = [element * self.interval for element in self.direction]
-                pipelineUtils.Pipeline.move (bpjnt , distance)
-                # 将bp关节添加到选择集里方便进行选择
-                pipelineUtils.Pipeline.create_set (self.bpjnt ,
-                                                   set_name = '{}_{}{}_bpjnt_set'.format (self.side , self.name ,
-                                                                                          self.rigType) ,
-                                                   set_parent = 'bpjnt_set')
+            self._create_single_bpjnt (bpjnt)
         # 进行关节定向
         jointUtils.Joint.joint_orientation (self.bpjnt_list)
         cmds.setAttr (self.bpjnt_list [0] + '.translateX' , 0)
 
 
-    def create_joint (self) :
-        '''
-        根据定位的bp关节创建关节
-        '''
-        # 设置定位关节的可见性
-        cmds.setAttr (self.bpjnt_list [0] + '.visibility' , 0)
-        # 根据bp关节创建新的关节
-        for bpjnt in self.bpjnt_list :
-            # 断掉上面的属性链接
-            connectionUtils.Connection.disconnect_attributes (bpjnt , ['.translate' , '.rotate' , '.scale'])
-
-        # 判断场景里是否已经存在对应的关节，重建的情况
-        if cmds.objExists (self.jnt_list [0]) :
-            # 删除过去的关节后，并重新创建关节
-            cmds.delete (self.jnt_list [0])
+    # 创建单个bp关节
+    def _create_single_bpjnt (self , bpjnt , index) :
+        """
+           创建单个bp关节
+           """
+        # 判断是否已经生成过定位关节，如果没有生成过定位关节的话则生成定位关节
+        if cmds.objExists (bpjnt) :
+            cmds.delete (bpjnt)
         else :
-            pass
+            self.bpjnt = cmds.createNode ('joint' , name = bpjnt , parent = self.bpjnt_parent)
+            # 给bp定位关节设置颜色方便识别
+            cmds.setAttr (self.bpjnt + '.overrideEnabled' , 1)
+            cmds.setAttr (self.bpjnt + '.overrideColor' , 13)
+
+            # 调整距离,调整关节的朝向距离
+            # 获取关节的位置信息，例如self.interval是关节的长度，self.direction是空间位置比如[0.0, 1, 0.0]
+            # 他们相乘获得到distance，将关节为位移到对应的位置信息上
+            distance = [element * self.interval for element in self.direction]
+            pipelineUtils.Pipeline.move (bpjnt , distance)
+
+            # 将bp关节添加到选择集里方便进行选择
+            pipelineUtils.Pipeline.create_set (self.bpjnt ,
+                                               set_name = '{}_{}{}_bpjnt_set'.format (self.side , self.name ,
+                                                                                      self.rigType) ,
+                                               set_parent = 'bpjnt_set')
+
+            # 指定关节的父层级为上一轮创建出来的关节
+            self.bpjnt_parent = self.bpjnt
+
+
+    ## 根据bpjnt的位置，创建对应的关节吸附到对应位置，并且创建用来整理的集合
+    def _create_and_attach_joint (self , bpjnt , jnt) :
         for bpjnt , jnt in zip (self.bpjnt_list , self.jnt_list) :
             # 场景里没有存在对应的关节，第一次创建绑定的情况
             self.jnt = cmds.createNode ('joint' , name = jnt , parent = self.jnt_parent)
@@ -139,6 +103,3 @@ class Chain (base.Base) :
                                                set_name = '{}_{}{}_jnt_set'.format (self.side , self.name ,
                                                                                     self.rigType) ,
                                                set_parent = 'jnt_set')
-
-        # 进行关节定向
-        jointUtils.Joint.joint_orientation (self.jnt_list)
