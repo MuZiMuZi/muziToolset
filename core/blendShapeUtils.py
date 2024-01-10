9f68a4db31f4e8dfb0074de76c827c6688ef7821
li xin <3318395483@qq.com>
2024-01-10 13:44:12 +0800

    BlendShape优化代码结构

--- core/blendShapeUtils.py
+++ core/blendShapeUtils.py
@@ -28,24 +28,28 @@ class BlendShape () :
         self.get_blendshape_weight_list ()
 
 
+    # 获取混合变形的节点
     def get_blendshape_node (self) :
         # 获取混合变形的节点
         self.blendshape_node = cmds.listConnections (self.model_node + '.worldMesh[0]' , type = 'blendShape')
         return self.blendshape_node
 
 
+    # 获取混合变形节点的名称
     def get_blendshape_name (self) :
         # 获取混合变形节点的名称
         self.blendshape_name = cmds.listAttr (self.blendshape_node [0] , m = True)
         return self.blendshape_name
 
 
+    # 获取混合变形节点的权重
     def get_blendshape_weight (self) :
         # 获取混合变形节点的权重
         self.blendshape_weight = cmds.getAttr (self.blendshape_node [0] + '.' + self.blendshape_name [0])
         return self.blendshape_weight
 
 
+    # 获取混合变形节点权重列表
     def get_blendshape_weight_list (self) :
         # 获取混合变形节点权重列表
         self.blendshape_weight_list = []
