98d0bfbf87befef374630c60afc51eb5dd629289
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-08-29 15:03:11 +0800

    refactor: remove reload from startup script

--- start.py
+++ start.py
@@ -1,12 +1,9 @@
 # coding=utf-8
-u"""Muzi Rigging 快速启动脚本。"""
+u"""Muzi Toolset 快速启动脚本。"""
 
 from __future__ import print_function
-
-from importlib import reload
 
 import muziToolset
 
 
-reload(muziToolset)
 muziToolset.show()
