b4f368db88be90635ccc6bf1a7649514d39bc246
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-08-29 22:19:16 +0800

    refactor: expose controller space blend api

--- systems/controller/__init__.py
+++ systems/controller/__init__.py
@@ -5,10 +5,14 @@ from .builder import create_controller
 from .builder import create_fk_controls
 from .builder import get_control_name_from_target
 from .builder import get_side_color
+from .space_blend import create_parent_space_blend
+from .space_blend import ensure_follow_attribute
 
 __all__ = [
     "create_controller",
     "create_fk_controls",
     "get_control_name_from_target",
     "get_side_color",
+    "create_parent_space_blend",
+    "ensure_follow_attribute",
 ]
