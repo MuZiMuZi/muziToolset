08a5db3ab5eedfeb85e9f6487d50d2867dec1491
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-08-29 12:33:46 +0800

    document controller tools package

--- MuziTools/tools/ctrl/__init__.py
+++ MuziTools/tools/ctrl/__init__.py
@@ -1,1 +1,10 @@
 # coding=utf-8
+u"""MuziTools 控制器工具包。"""
+
+__all__ = [
+    "control_shape_tool",
+    "create_ctrl_tool",
+    "create_fk_ctrl_tool",
+    "set_ctrl_tool",
+    "face_rig_tool",
+]
