8c6c61ced5db507602924f0ac4e42206c46d8f11
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-08-31 00:33:10 +0800

    refactor(face): add face data package

---
+++ systems/face/data/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+u"""Face Rig 公共数据定义。"""
+
+from __future__ import print_function
+
+from .shape_dictionary import face_shape_dictionary
+
+
+__all__ = [
+    "face_shape_dictionary",
+]
