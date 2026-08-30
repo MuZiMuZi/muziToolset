6d6e435973ea201f895f03153c08e70de075bf3f
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-08-30 23:50:42 +0800

    feat(ui): expose maya index color slider

--- ui/widgets/__init__.py
+++ ui/widgets/__init__.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 u"""Muzi Rigging 可复用 UI 组件。"""
 
+from .color_index_slider import MayaIndexColorSlider
 from .object_picker import MayaObjectPicker
 
 
 __all__ = [
+    "MayaIndexColorSlider",
     "MayaObjectPicker",
 ]
