c8fb29cccaed72e78760219274110667dd6e730c
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-08-31 15:23:44 +0800

    Expose unified component base classes

--- systems/__init__.py
+++ systems/__init__.py
@@ -1,2 +1,11 @@
 # coding=utf-8
 u"""完整绑定系统集合。"""
+
+from .component_base import ComponentBase
+from .component_base import RigComponentBase
+
+
+__all__ = [
+    "ComponentBase",
+    "RigComponentBase",
+]
