4e8a37e0abc5fbf9f45e61f20b0673521ff81f47
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-08-31 13:24:17 +0800

    refactor(face): expose only face guide

--- systems/face/guide/__init__.py
+++ systems/face/guide/__init__.py
@@ -4,14 +4,8 @@ u"""Face Rig Step 02 - Guide。"""
 from __future__ import print_function
 
 from .face_guide import FaceGuide
-from .guide_mirror import mirror_guides
-from .guide_mirror import undo_mirror
-from .guide_template import reimport_template_preserve_guide
 
 
 __all__ = [
     "FaceGuide",
-    "mirror_guides",
-    "undo_mirror",
-    "reimport_template_preserve_guide",
 ]
