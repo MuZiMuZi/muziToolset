824a335ecfcd00a4e00786e322d1ca7dbc354147
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-09-02 13:34:42 +0800

    Add Face modules package

---
+++ systems/face/modules/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+u"""Face Rig Step 03 Modules。"""
+
+from __future__ import print_function
+
+from .teeth import TeethModule
+from .teeth import build_teeth
+
+
+__all__ = [
+    "TeethModule",
+    "build_teeth",
+]
