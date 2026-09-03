ac37dc08902c08024d9225bbcb3e3994b5d8eabb
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-09-03 10:25:24 +0800

    Document final Face module lifecycle

--- systems/face/modules/__init__.py
+++ systems/face/modules/__init__.py
@@ -1,5 +1,19 @@
 # coding=utf-8
-u"""Face Rig Step 03 正式 Modules。"""
+u"""
+Face Rig Step 03 正式 Modules。
+
+所有 Face Module 统一使用：
+    load_setup()
+    load_guide()
+    create_jnt()
+    create_ctrl()
+    create_connect()
+    create_deform()
+    create_finalize()
+    create_build()
+
+旧 Face Module 生命周期不再作为正式 API 维护。
+"""
 
 from __future__ import print_function
 
