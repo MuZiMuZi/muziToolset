fc4811d0646089131d74b082b5cb6105820bfc6f
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-09-03 10:54:28 +0800

    Expose all modular Face rig modules

--- systems/face/modules/__init__.py
+++ systems/face/modules/__init__.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 u"""
-Face Rig Step 03 正式 Modules。
+Face Rig 正式 Modules
+=====================
 
-所有 Face Module 统一使用：
+所有独立 Face Rig Module 统一使用：
     load_setup()
     load_guide()
     create_jnt()
@@ -17,17 +18,57 @@ Face Rig Step 03 正式 Modules。
 
 from __future__ import print_function
 
+from .brow import BrowModule
+from .brow import build_brow
+from .cheek import CheekModule
+from .cheek import build_cheek
+from .ear import EarModule
+from .ear import build_ear
+from .eye import EyeModule
+from .eye import build_eye
+from .eyelid import EyelidModule
+from .eyelid import build_eyelid
 from .face_module_base import FaceModuleBase
+from .face_rig import FaceRig
+from .face_rig import build_face
 from .jaw import JawModule
 from .jaw import build_jaw
+from .lip import LipModule
+from .lip import build_lip
+from .mouth import MouthModule
+from .mouth import build_mouth
+from .nose import NoseModule
+from .nose import build_nose
 from .teeth import TeethModule
 from .teeth import build_teeth
+from .tongue import TongueModule
+from .tongue import build_tongue
 
 
 __all__ = [
     "FaceModuleBase",
+    "FaceRig",
+    "BrowModule",
+    "CheekModule",
+    "EarModule",
+    "EyeModule",
+    "EyelidModule",
     "JawModule",
+    "LipModule",
+    "MouthModule",
+    "NoseModule",
     "TeethModule",
+    "TongueModule",
+    "build_face",
+    "build_brow",
+    "build_cheek",
+    "build_ear",
+    "build_eye",
+    "build_eyelid",
     "build_jaw",
+    "build_lip",
+    "build_mouth",
+    "build_nose",
     "build_teeth",
+    "build_tongue",
 ]
