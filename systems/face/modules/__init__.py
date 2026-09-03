590ed402628f764fa6adbc750eb4545ad29e83c1
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-09-03 10:16:30 +0800

    Expose unified face modules

--- systems/face/modules/__init__.py
+++ systems/face/modules/__init__.py
@@ -1,13 +1,19 @@
 # coding=utf-8
-u"""Face Rig Step 03 Modules。"""
+u"""Face Rig Step 03 正式 Modules。"""
 
 from __future__ import print_function
 
+from .face_module_base import FaceModuleBase
+from .jaw import JawModule
+from .jaw import build_jaw
 from .teeth import TeethModule
 from .teeth import build_teeth
 
 
 __all__ = [
+    "FaceModuleBase",
+    "JawModule",
     "TeethModule",
+    "build_jaw",
     "build_teeth",
 ]
