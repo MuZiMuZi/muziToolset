4606d20bb7797a4bcd3d3b1aea73121e455a8429
muzi <3318395383@qq.com>
2026-09-04 18:17:56 +0800

    更新层级工具的方法，新增了chain_parent

--- core/common/hierarchy_utils.py
+++ core/common/hierarchy_utils.py
@@ -24,3 +24,35 @@ def chain_parent (child_nodes , parent_node) :
     for child_node in child_nodes :
        parent(child_node , parent_node)
        parent_node = child_node
+
+def add_extra_group (obj , grp_name , world_orient = False) :
+    u"""在对象上方添加一个额外的组.
+
+    Args:
+        obj (str):要添加额外组的Maya对象.
+        grp_name (str): 额外的组名
+        world_orient (bool): 设置新组的世界位置是否改变。
+
+    Returns:
+        str: 新添加的组.
+
+    """
+    #创建一个组
+    obj_grp = cmds.group (name = grp_name , empty = True)
+    t_pos = cmds.xform (obj , query = True , worldSpace = True , translation = True)
+    r_pos = cmds.xform (obj , query = True , worldSpace = True , rotation = True)
+    if world_orient :
+        r_pos = [0 , 0 , 0]
+    s_pos = cmds.xform (obj , q = True , worldSpace = True , s = True)
+    cmds.xform (obj_grp , s = s_pos)
+    cmds.xform (obj_grp , ws = True , t = t_pos)
+    cmds.xform (obj_grp , ws = True , ro = r_pos)
+
+    obj_parent = cmds.listRelatives (obj , parent = True)
+    if obj_parent :
+        cmds.parent (obj_grp , obj_parent [0] , absolute = True)
+        cmds.parent (obj , obj_grp , absolute = True)
+    else :
+        cmds.parent (obj , obj_grp , absolute = True)
+
+    return obj_grp
