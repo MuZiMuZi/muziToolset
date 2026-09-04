874ba212735ceda9c059601629fd23317e8e0482
muzi <3318395383@qq.com>
2026-09-04 23:23:26 +0800

    完善 attr_utils 中文注释并修正世界矩阵保存

--- core/common/attr_utils.py
+++ core/common/attr_utils.py
@@ -9,8 +9,34 @@ class Attr(object):
         if object:
             self.object = pm.PyNode(object)
 
+
+    def has_attr (self , attr_name) :
+        u"""
+        检查当前对象是否存在指定属性。
+
+        attr_name(str): 需要检查的属性名称。
+
+        Returns:
+            bool: 属性存在返回 True，不存在返回 False。
+
+        Maya 使用示例：
+
+        from muziToolset.core.common import attr_utils
+
+        attr_object = attr_utils.Attr("ctrl_lf_eye_main_001")
+
+        result = attr_object.has_attr("follow")
+
+        print(result)
+        """
+        has_attr_value = self.object.hasAttr (attr_name)
+        return has_attr_value
+
     def add_attr(self, attr_name, attr_type="double", default_value=0, keyable=True):
-        self.object.addAttr(attr_name, attributeType=attr_type, defaultValue=default_value, keyable=keyable)
+        if self.object.has_attr():
+            return
+        else:
+            self.object.addAttr(attr_name, attributeType=attr_type, defaultValue=default_value, keyable=keyable)
 
     def set_value(self, attr_name, value):
         self.object.setAttr(attr_name, value)
@@ -34,3 +60,9 @@ class Attr(object):
     def lock_hide_attr(self, attr_name):
         self.lock_attr(attr_name)
         self.hide_attr(attr_name)
+
+    def connect_attr(self, attr_name, target_object,target_attr_name):
+        pm.connectAttr(self.object.attr(attr_name), target_object.attr(target_attr_name), force=True)
+
+    def disconnect_attr(self, attr_name, target_object,target_attr_name):
+        pm.disconnectAttr(self.object.attr(attr_name), target_object.attr(target_attr_name))
