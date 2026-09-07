62712148b210f92092f7bc963c3fb0dc471ddbe2
muzi <3318395383@qq.com>
2026-09-07 19:44:05 +0800

    修改了rig_base和ear.py

--- systems/face/ear.py
+++ systems/face/ear.py
@@ -1,4 +1,6 @@
 import maya.cmds as cmds
+
+from common import name_utils
 from systems import rig_module
 from importlib import reload
 form ....core.common  import *
@@ -9,10 +11,10 @@ class Ear(rig_module.RigModule):
         super().__init__(module, side, guide, jnt_parent,ctrl_parent)
 
     def create_name(self):
-
-        jnt_names =
-
-        pass
+        self.jnt_names_list = []
+        for index in  range(3):
+            self.jnt_names = name_utils.Name(type = 'jnt',side = self.side,part = 'ear',function = 'bind',index = index)
+            self.jnt_names_list.append(self.jnt_names)
 
     def create_joint(self):
         pass
