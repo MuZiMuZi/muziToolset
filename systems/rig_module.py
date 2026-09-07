d55d1c2494ca5e5e8a4a47362c43abe57c8fdfc7
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-09-07 19:51:17 +0800

    简化 RigModule 构建生命周期

--- systems/rig_module.py
+++ systems/rig_module.py
@@ -3,21 +3,15 @@ import pymel.core as pm
 
 
 class RigModule(object):
-    def __init__(self, module = None, side = 'md', guide=None, jnt_parent=None,ctrl_parent=None):
+    def __init__(self, module=None, side='md', guide=None, jnt_parent=None, ctrl_parent=None):
         self.module = module
         self.side = side
         self.guide = guide
         self.jnt_parent = jnt_parent
         self.ctrl_parent = ctrl_parent
 
-
-
-    def create_names(self):
-        pass
-
     def get_guides(self):
         pass
-
 
     def create_joints(self):
         pass
@@ -32,8 +26,8 @@ class RigModule(object):
         pass
 
     def build(self):
-        self.create_names()
+        self.get_guides()
         self.create_joints()
         self.create_controls()
         self.connect_rig()
-
+        self.setup_hierarchy()
