b09b6e528025d6b0afb3364f0525ca036a794c70
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-09-07 19:51:40 +0800

    将 Ear 名称创建并入关节阶段

--- systems/face/ear_module.py
+++ systems/face/ear_module.py
@@ -1,21 +1,27 @@
 import maya.cmds as cmds
 
-from common import name_utils
-from systems import rig_module
-from importlib import reload
-form ....core.common  import *
-reload(rig_module)
+from .. import rig_module
+from ...core.common import name_utils
+
 
 class EarModule(rig_module.RigModule):
-    def __init__(self,module, side, guide, jnt_parent,ctrl_parent):
-        super().__init__(module, side, guide, jnt_parent,ctrl_parent)
 
-    def create_name(self):
-        self.jnt_names_list = []
-        for index in  range(3):
-            self.jnt_names = name_utils.Name(type = 'jnt',side = self.side,part = 'ear',function = 'bind',index = index)
-            self.jnt_names_list.append(self.jnt_names)
+    def __init__(self, module, side, guide, jnt_parent, ctrl_parent):
+        super().__init__(module, side, guide, jnt_parent, ctrl_parent)
 
-    def create_joint(self):
-        pass
+    def create_joints(self):
+        self.jnt_names = []
 
+        for index in range(1, 4):
+            name_object = name_utils.Name(
+                type="jnt",
+                side=self.side,
+                part=self.module,
+                function="bind",
+                index=index
+            )
+
+            jnt_name = name_object.compose_name()
+            self.jnt_names.append(jnt_name)
+
+        return self.jnt_names
