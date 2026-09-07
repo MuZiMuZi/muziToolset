c8e6119e89b1cadf5f5b47a3de49da30e2274a0d
MuZiMuZi <114722174+MuZiMuZi@users.noreply.github.com>
2026-09-07 19:33:58 +0800

    重命名 Rig 基类文件与构建方法

---
+++ systems/rig_module.py
@@ -0,0 +1,39 @@
+import maya.cmds as cmds
+import pymel.core as pm
+
+
+class RigBase(object):
+    def __init__(self, module = None, side = 'md', guide=None, jnt_parent=None,ctrl_parent=None):
+        self.module = module
+        self.side = side
+        self.guide = guide
+        self.jnt_parent = jnt_parent
+        self.ctrl_parent = ctrl_parent
+
+
+
+    def create_names(self):
+        pass
+
+    def get_guides(self):
+        pass
+
+
+    def create_joints(self):
+        pass
+
+    def create_controls(self):
+        pass
+
+    def connect_rig(self):
+        pass
+
+    def setup_hierarchy(self):
+        pass
+
+    def build(self):
+        self.create_names()
+        self.create_joints()
+        self.create_controls()
+        self.connect_rig()
+
